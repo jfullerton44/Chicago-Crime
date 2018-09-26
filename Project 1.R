fb6d2c782b6db3131f771f92ae852d09f666862e
Jake Fullerton <fullertonj44@gmail.com>
2018-09-26 14:42:34 -0400

    Read and bind

--- Project 1.R
+++ Project 1.R
@@ -1,2 +1,7 @@
 setwd("/Users/blake/Documents/UVA/Fall 2018/DS 4001/Project 1")
+setwd("~/Desktop/DS/Project1")
+to2004 <- read.csv("Chicago_Crimes_2001_to_2004.csv")
+to2007 <- read.csv("Chicago_Crimes_2005_to_2007.csv")
+to2011 <- read.csv("Chicago_Crimes_2008_to_2011.csv")
+to2017 <- read.csv("Chicago_Crimes_2012_to_2017.csv")
 
