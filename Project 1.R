5287ee3e0c544abc5ab4ad52886131d841fe6174
Jake Fullerton <fullertonj44@gmail.com>
2018-09-26 15:24:41 -0400

    add bind

--- Project 1.R
+++ Project 1.R
@@ -4,4 +4,4 @@ to2004 <- read.csv("Chicago_Crimes_2001_to_2004.csv")
 to2007 <- read.csv("Chicago_Crimes_2005_to_2007.csv")
 to2011 <- read.csv("Chicago_Crimes_2008_to_2011.csv")
 to2017 <- read.csv("Chicago_Crimes_2012_to_2017.csv")
-
+all <- rbind(to2004,to2007,to2011,to2017)
